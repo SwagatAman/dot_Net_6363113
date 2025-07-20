1e73562be9997bbbb25bbd1c5906bbe9222f7a1a
SwagatAman <156504348+SwagatAman@users.noreply.github.com>
2025-07-20 23:21:08 +0530

    Delete Week_5/Microservices/WebApi-6 directory

--- Week_5/Microservices/WebApi-6/WebApi-6_HandOn_1/ChatAppSolution/ChatApp.Common/Models/ChatMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatApp.Common.Models
-{
-    public class ChatMessage
-    {
-        public string Sender { get; set; }
-        public string Message { get; set; }
-        public DateTime Timestamp { get; set; }
-    }
-}
--- Week_5/Microservices/WebApi-6/WebApi-6_HandsOn_2/ChatAppSolution/ChatApp.Kafka/Interfaces/IKafkaConsumerCallback.cs
+++
@@ -1,7 +0,0 @@
-namespace ChatApp.Kafka.Interfaces
-{
-    public interface IKafkaConsumerCallback
-    {
-        void OnMessageReceived(string message);
-    }
-}
